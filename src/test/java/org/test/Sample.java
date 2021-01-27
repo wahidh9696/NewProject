92991001cb69877b96ca239bd1c9c99802bad80c
wahidh9696 <abdulwahidh9696@gmail.com>
2021-01-27 10:08:55 +0530

    new class

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+	}
+
+}
